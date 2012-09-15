5f3a97061a35b053cab322b1d6dbd4414a12a2ee
Jessie Mao <jessiemao@quad2-044.wireless-resnet.upenn.edu>
2012-09-15 03:48:58 -0400

    Fixed login, added main function

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
 var numEvents;
-function processEvents(response) {console.log(response)};
-FB.api('/me/events', processEvents);
+var eventArray = new Array();
+
+function processEvents(response) {console.log(response);
+	for (var i = 0; i < numEvents; i++) {
+		eventArray[i] = response.data[i].name;
+	}
+	console.log(eventArray);
+};
+
+function main() {
+	FB.api('/me/events', processEvents);
+}
+
+
 
 /*
 // creates array and adds all the user's events to it
-var eventArray = new Array();
 var addEventsToArray = function() {
 	for (var i = 0; i < numEvents; i++) {
 		FB.api('/me/events', function(response) {eventArray[i] = response.data[i].id});
